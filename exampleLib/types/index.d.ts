8ec21aeaf02762f8ea15c7733f5fd87868ddca96
Arne Küderle <a.kuederle@gmail.com>
2024-10-08 11:38:22 +0200

    Rerun dts-buddy

--- exampleLib/types/index.d.ts
+++ exampleLib/types/index.d.ts
@@ -32,10 +32,36 @@ declare module 'exampleLib' {
 }
 
 declare module 'exampleLib/format' {
-	import { Person, PersonNoPrivate } from 'exampleLib';
-
 	export function formatPerson(person: Person): string;
 	export function formatPersonNoPrivate(person: PersonNoPrivate): string;
+	class Person {
+		/**
+		 * Create a person object.
+		 * @param name - The name of the person.
+		 * @param age - The age of the person.
+		 * @param email - The email address of the person.
+		 */
+		constructor(name: string, age: number, email: string);
+		name: string;
+		age: number;
+		email: string;
+		
+		get id(): string;
+		#private;
+	}
+	class PersonNoPrivate {
+		/**
+		 * Create a person object.
+		 * @param name - The name of the person.
+		 * @param age - The age of the person.
+		 * @param email - The email address of the person.
+		 */
+		constructor(name: string, age: number, email: string);
+		name: string;
+		age: number;
+		email: string;
+		id: string;
+	}
 
 	export {};
 }
